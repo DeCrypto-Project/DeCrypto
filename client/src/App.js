5d194849dfe48266a2d15192008f21dbcbb923a9
Nir Jacob <70593188+nirjacob@users.noreply.github.com>
2022-07-27 20:47:45 +0300

    * working unedited website

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,82 @@
-import './App.css'
-import React from 'react'
-import { BrowserRouter as Router, Navigate, Route, Routes } from 'react-router-dom'
-import Home from './containers/Home/Home'
+import { lazy, Suspense, useEffect } from 'react';
 
-function App() {
-  return (
-    <div className='App'>
-      <Router>
-        <Routes>
-          <Route path='/home' element={<Home/>} />
-        </Routes>
-      </Router>
-    </div>
-  )
-}
+/// Components
+import Index from "./jsx";
+import { connect, useDispatch } from 'react-redux';
+import {  Route, Switch, withRouter } from 'react-router-dom';
+// action
+import { checkAutoLogin } from './services/AuthService';
+import { isAuthenticated } from './store/selectors/AuthSelectors';
+/// Style
+import './vendor/swiper/swiper-bundle.css';
+import "./vendor/bootstrap-select/dist/css/bootstrap-select.min.css";
+import "./css/style.css";
 
-export default App
+
+const SignUp = lazy(() => import('./jsx/pages/Registration'));
+const ForgotPassword = lazy(() => import('./jsx/pages/ForgotPassword'));
+const Login = lazy(() => {
+    return new Promise(resolve => {
+    setTimeout(() => resolve(import('./jsx/pages/Login')), 500);
+  });
+});
+
+function App (props) {
+    const dispatch = useDispatch();
+    useEffect(() => {
+        checkAutoLogin(dispatch, props.history);
+    }, [dispatch, props.history]);
+    
+    let routes = (  
+        <Switch>
+            <Route path='/login' component={Login} />
+            <Route path='/page-register' component={SignUp} />
+            <Route path='/page-forgot-password' component={ForgotPassword} />
+        </Switch>
+    );
+    if (props.isAuthenticated) {
+		return (
+			<>
+                <Suspense fallback={
+                    <div id="preloader">
+                        <div className="sk-three-bounce">
+                            <div className="sk-child sk-bounce1"></div>
+                            <div className="sk-child sk-bounce2"></div>
+                            <div className="sk-child sk-bounce3"></div>
+                        </div>
+                    </div>  
+                   }
+                >
+                    <Index / >
+                </Suspense>
+            </>
+        );
+	
+	}else{
+		return (
+			<div className="vh-100">
+                <Suspense fallback={
+                    <div id="preloader">
+                        <div className="sk-three-bounce">
+                            <div className="sk-child sk-bounce1"></div>
+                            <div className="sk-child sk-bounce2"></div>
+                            <div className="sk-child sk-bounce3"></div>
+                        </div>
+                    </div>
+                  }
+                >
+                    {routes}
+                </Suspense>
+			</div>
+		);
+	}
+};
+
+const mapStateToProps = (state) => {
+    return {
+        isAuthenticated: isAuthenticated(state),
+    };
+};
+
+export default withRouter(connect(mapStateToProps)(App)); 
+
--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { BrowserRouter } from 'react-router-dom';
+import {Provider} from 'react-redux';
+import {store} from './store/store';
+import reportWebVitals from "./reportWebVitals";
+import SimpleReactLightbox from "simple-react-lightbox";
+import  ThemeContext  from "./context/ThemeContext"; 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+	<React.StrictMode>
+		<Provider store = {store}>
+            <SimpleReactLightbox>
+                <BrowserRouter basename='/react/demo'>
+                    <ThemeContext>
+                        <App />
+                    </ThemeContext>  
+                </BrowserRouter>    
+            </SimpleReactLightbox>
+        </Provider>	
+	</React.StrictMode>,
+  document.getElementById("root")
+);
+reportWebVitals();
